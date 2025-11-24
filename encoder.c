edfcc5eb8fb6325bb71d33b0423cdbea45c8de4a
Admin <cychiang@mail.ntpu.edu.tw>
2025-11-24 14:54:21 +0800

    Add print_codebook function, README.md, .gitignore and GitHub Actions workflow

--- encoder.c
+++ encoder.c
@@ -18,12 +18,40 @@ typedef struct _huffman_node {
     char symbol;
     float probability;
     struct _huffman_node *left;
-    struct _huffman_node *right
+    struct _huffman_node *right;
 } huffman_node;
 
 typedef struct _huffman_tree {
     huffman_node *root;
 } huffman_tree;
+
+void print_codebook(codebook *cb) {
+    printf("\n========== Codebook Contents ==========\n");
+    printf("%-8s %-8s %-12s %-10s %-18s\n", 
+           "Symbol", "Count", "Probability", "Bits", "Self-Information");
+    printf("-----------------------------------------------------------\n");
+    
+    for (size_t i = 0; i < cb->size; i++) {
+        char symbol_display[10];
+        if (cb->words[i].symbol == ' ') {
+            sprintf(symbol_display, "<space>");
+        } else if (cb->words[i].symbol == '\n') {
+            sprintf(symbol_display, "<LF>");
+        } else if (cb->words[i].symbol == '\r') {
+            sprintf(symbol_display, "<CR>");
+        } else {
+            sprintf(symbol_display, "%c", cb->words[i].symbol);
+        }
+        
+        printf("%-8s %-8d %-12.6f %-10s %-18.6f\n",
+               symbol_display,
+               cb->words[i].count,
+               cb->words[i].probability,
+               cb->words[i].bits,
+               cb->words[i].self_information);
+    }
+    printf("========================================\n\n");
+}
 
 int build_huffman_tree(huffman_tree *tree, codebook *cb) {
     // Implementation of Huffman tree building (not shown here)
@@ -56,6 +84,9 @@ int main() {
     cb.words[17] = (codeword){'o', 4, 0.085106000000000, "0010", 3.554595343819928};
     cb.words[18] = (codeword){' ', 5, 0.106383000000000, "100", 3.232660468251296};
     cb.words[19] = (codeword){'n', 5, 0.106383000000000, "101", 3.232660468251296};
+
+    // Display the codebook
+    print_codebook(&cb);
 
     // conduct Huffman encoding on input data (not implemented here)
     build_huffman_tree(&ht, &cb);
